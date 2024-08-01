395b28d5a7b9d7d3172a73262738191884249f52
Gwalchaved-dev <hunault@hotmail.fr>
2024-08-01 17:46:47 +0200

    base des fichiers de routage à ajuster à notre site

---
+++ Router/allRoutes.js
@@ -0,0 +1,15 @@
+import Route from "./Route.js";
+
+//Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", [], "/js/galerie.js"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon espace", "/pages/auth/account.html", ["client", "admin, employee"]),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html", ["client", "admin"]),
+    new Route("/allResa", "Vos achats", "/pages/reservations/allResa.html", ["client"]),
+];
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Gamestore";
