a6039857a0b0d63ed8f900b8a955968090dac63f
Gwalchaved-dev <hunault@hotmail.fr>
2024-09-17 15:42:57 +0200

    config mailer + debug carousel ok

--- Assets/carousel.js
+++ Assets/carousel.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var carousel = document.querySelector('#monCarousel');
-    
-    // Active Bootstrap carousel
-    var bsCarousel = new bootstrap.Carousel(carousel, {
-      interval: 3000,  // 3 secondes entre chaque slide
-      ride: 'carousel'
-    });
-  
-    // Fonctionnalité des boutons de contrôle
-    document.querySelector('.carousel-control-prev').addEventListener('click', function () {
-      bsCarousel.prev();
-    });
-  
-    document.querySelector('.carousel-control-next').addEventListener('click', function () {
-      bsCarousel.next();
-    });
-  });
+  var carousel = document.querySelector('#monCarousel');
+
+  if (carousel) {
+      // Active Bootstrap carousel avec les options souhaitées
+      var bsCarousel = new bootstrap.Carousel(carousel, {
+          interval: 3000,  // 3 secondes entre chaque slide
+          ride: 'carousel'
+      });
+
+      // Vérifier et ajouter les événements sur les boutons de contrôle
+      var prevButton = document.querySelector('.carousel-control-prev');
+      var nextButton = document.querySelector('.carousel-control-next');
+
+      if (prevButton) {
+          prevButton.addEventListener('click', function () {
+              bsCarousel.prev();
+          });
+      }
+
+      if (nextButton) {
+          nextButton.addEventListener('click', function () {
+              bsCarousel.next();
+          });
+      }
+  }
+});
