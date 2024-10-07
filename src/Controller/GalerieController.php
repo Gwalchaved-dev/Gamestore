e80cc0256b4d84df23a80af89ad888d518f07541
Gwalchaved-dev <hunault@hotmail.fr>
2024-10-07 15:36:28 +0200

    modif galerie ok et preparation mise en place panier

--- src/Controller/GalerieController.php
+++ src/Controller/GalerieController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route; // Remplace `Attribute\Route` par `Annotation\Route`
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\JeuxVideosRepository;
 
 class GalerieController extends AbstractController
@@ -18,10 +18,15 @@ class GalerieController extends AbstractController
         $minPrice = $request->query->get('min_price');
         $maxPrice = $request->query->get('max_price');
 
-        // Construire la requête de filtrage
-        $jeux = $jeuxRepository->findByFilters($genre, $minPrice, $maxPrice);
+        // Appeler la méthode de repository avec filtrage si nécessaire
+        if ($genre || $minPrice || $maxPrice) {
+            $jeux = $jeuxRepository->findByFilters($genre, $minPrice, $maxPrice);
+        } else {
+            // Si pas de filtre, récupérer tous les jeux
+            $jeux = $jeuxRepository->findAll();
+        }
 
-        // Renvoyer la vue avec les jeux filtrés
+        // Renvoyer la vue avec les jeux récupérés (filtrés ou non)
         return $this->render('galerie/galerie.html.twig', [
             'jeux' => $jeux,
         ]);
