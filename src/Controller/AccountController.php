41ce1d8244a27d83892a0261f1fae49a86a852f4
Gwalchaved-dev <hunault@hotmail.fr>
2024-10-22 15:56:56 +0200

    mise en page final du compte client, plus fonctionnalité modification informations et changement mot de passe

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,35 +26,75 @@ class AccountController extends AbstractController
 
         // Redirige les administrateurs vers la page admin
         if ($this->isGranted('ROLE_ADMIN')) {
-            return $this->redirectToRoute('app_admin'); // Redirection vers la route de l'espace admin
-        }
-
-        // Création du formulaire à partir de RegistrationFormType
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Persist user changes
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Informations mises à jour avec succès.');
-            return $this->redirectToRoute('account');
+            return $this->redirectToRoute('app_admin');
         }
 
         return $this->render('account/account.html.twig', [
-            'form' => $form->createView(),
             'user' => $user,
         ]);
     }
 
-    #[Route('/change-password', name: 'change_password', methods: ['POST'])]
+    #[Route('/account/edit/{field}', name: 'account_edit')]
+    public function editField(Request $request, string $field, Security $security, EntityManagerInterface $entityManager): Response
+    {
+        $user = $security->getUser();
+
+        if (!$user || !$user instanceof User) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Liste des champs autorisés à être modifiés
+        $allowedFields = ['email', 'nom', 'prenom', 'adresse_postale', 'codepostal', 'ville'];
+        
+        if (!in_array($field, $allowedFields)) {
+            $this->addFlash('error', 'Modification non autorisée.');
+            return $this->redirectToRoute('account');
+        }
+
+        if ($request->isMethod('POST')) {
+            $newValue = $request->request->get('new_value');
+
+            switch ($field) {
+                case 'email':
+                    $user->setEmail($newValue);
+                    break;
+                case 'nom':
+                    $user->setNom($newValue);
+                    break;
+                case 'prenom':
+                    $user->setPrenom($newValue);
+                    break;
+                case 'adresse_postale':
+                    $user->setAdressePostale($newValue);  // Modification ici pour utiliser `adresse_postale`
+                    break;
+                case 'codepostal':
+                    $user->setCodepostal($newValue);
+                    break;
+                case 'ville':
+                    $user->setVille($newValue);
+                    break;
+            }
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', ucfirst($field) . ' modifié avec succès.');
+            return $this->redirectToRoute('account');
+        }
+
+        return $this->render('account/edit_field.html.twig', [
+            'user' => $user,
+            'field' => $field,
+        ]);
+    }
+
+    #[Route('/account/change-password', name: 'change_password', methods: ['POST'])]
     public function changePassword(Request $request, UserPasswordHasherInterface $passwordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
         $user = $security->getUser();
 
         if (!$user || !$user instanceof User) {
-            return $this->json(['error' => 'User not logged in'], 403);
+            return $this->redirectToRoute('app_login');
         }
 
         $newPassword = $request->request->get('new_password');
@@ -69,14 +108,16 @@ class AccountController extends AbstractController
         // Assurez-vous que $user implémente PasswordAuthenticatedUserInterface
         if ($user instanceof PasswordAuthenticatedUserInterface) {
             // Encodage et mise à jour du mot de passe
-            $user->setPassword($passwordHasher->hashPassword($user, $newPassword));
+            $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+            $user->setPassword($hashedPassword);
+
+            // Persiste l'utilisateur avec le nouveau mot de passe
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Mot de passe changé avec succès.');
         }
 
-        // Persist the user with the new password
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        $this->addFlash('success', 'Mot de passe changé avec succès.');
         return $this->redirectToRoute('account');
     }
 }
